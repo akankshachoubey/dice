60d626e8379e15fb670b7312c8ed699d1bccbeb7
akankshachoubey <126176198+akankshachoubey@users.noreply.github.com>
2024-01-06 23:25:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1=Math.floor(Math.random()*6) + 1;
-var randomDice1="./dice"+randomNumber1+".png";
+var randomDice1="./"+randomNumber1+".png";
 document.querySelectorAll("img")[0].setAttribute("src",randomDice1);
 
 var randomNumber2=Math.floor(Math.random()*6) + 1;
-var randomDice2="./dice"+randomNumber1+".png";
+var randomDice2="./"+randomNumber1+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomDice2);
 
 if(randomNumber1>randomNumber2){
